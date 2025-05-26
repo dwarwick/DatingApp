a46a461c347c4427ce38560036c9e684bec099fc
David Warwick <bgmsd@live.com>
2025-05-26 11:50:43 -0700

    Enhance UI with MudBlazor components and styles
    Added MudBlazor components and styles to improve the DatingApp's user interface. Updated `App.razor` to include MudBlazor stylesheets and scripts. Refactored `MainLayout.razor` to use `CascadingAuthenticationState` and a new `MyNavMenu` component for navigation.
    
    Implemented a responsive navigation menu in `MyNavMenu.razor` with dark theme toggling and user authentication links. Updated `BlazorBase.cs` to inject necessary services and modified `_Imports.razor` for authentication support.
    
    Adjusted CSS in `app.css` and `MyNavMenu.razor.css` for proper layout and styling, ensuring content is not overlapped by the app bar. Overall, these changes significantly enhance the user experience and interface of the application.

---
+++ DatingApp/Components/BlazorBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+
+namespace DatingApp.Components
+{
+    public class BlazorBase : ComponentBase
+    {
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+        [Inject]
+        internal NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        internal ISnackbar Snackbar { get; set; }
+
+
+        [Inject]
+        internal IConfiguration Configuration { get; set; }
+
+        [Inject]
+        internal HttpClient Client { get; set; }
+
+        [Inject]
+        internal NavigationManager Navigation { get; set; }
+
+        [Inject]
+        internal IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        internal IDialogService MudDialog { get; set; }
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider adding the 'required' modifier or declaring as nullable.
+    }
+}
